90d9f1f1113b463fcf3f671e12ecfea079cddf1d
marcdragon123 <thedragon_master@hotmail.ca>
2016-03-15 21:39:00 -0400

    login

---
+++ SOEN341_PROJECT/SOEN341_PROJECT/login.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'functions.php';
+
+$link = getCon();
+
+$email = $_POST[inputEmail];
+$pass = $_POST[inputPassword];
+$result = $link->query($link, $query1 = "select email, password( ' + $pass + ') = password from student where ' + $email + ' = email; " );
+$errorstr = "Sorry could not login, invalid password or username. Please resubmit with the right login.";
+
+if($result){
+	link($target = "../Account.html" , $link = "Account");
+}
+else{
+	$_POST['error_msg'] = $errorstr;
+	header('Location = ../SignIn.html');
+}
+	
+?>	
+	
+	
