b99502378c44fb14084e27ff5f12d2b95698e763
wolfcall <gmathieu6@gmail.com>
2016-03-15 23:09:22 -0400

    Not working locally

--- DBScripts/tmpscript.sql
+++ DBScripts/tmpscript.sql
@@ -6,94 +6,94 @@ Select * from student;
 Show columns from student;
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','GMASASASASA', password('BLAH'), 'gm@email.com');
+values ('Georges','Mathieu','GMASASASASA', password('BLAHBLAH'), 'gm@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Adam','Arcaro','AAASASASASA', password('BLAH'), 'aa@email.com');
+values ('Adam','Arcaro','AAASASASASA', password('BLAHBLAH'), 'aa@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Hassan','Somehting','HSASASASASA', password('BLAH'),'hs@email.com');
+values ('Hassan','Somehting','HSASASASASA', password('BLAHBLAH'),'hs@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('George','SomethingwithaT','GTASASASASA', password('BLAH'),'gt@email.com');
+values ('George','SomethingwithaT','GTASASASASA', password('BLAHBLAH'),'gt@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Marc-Andre','Dragon','MDASASASASA', password('BLAH'),'md@email.com');
+values ('Marc-Andre','Dragon','MDASASASASA', password('BLAHBLAH'),'md@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Stefano','Pace','SPASASASASA', password('BLAH'),'sp@email.com');
+values ('Stefano','Pace','SPASASASASA', password('BLAHBLAH'),'sp@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Olivier','CC','OCCASASASASA', password('BLAH'),'oc@email.com');
+values ('Olivier','CC','OCCASASASASA', password('BLAHBLAH'),'oc@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Joe','Tedeschi','JTASASASASA', password('BLAH'),'jt@email.com');
+values ('Joe','Tedeschi','JTASASASASA', password('BLAHBLAH'),'jt@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Julian','Ippolito','JIASASASASA', password('BLAH'),'ji@email.com');
+values ('Julian','Ippolito','JIASASASASA', password('BLAHBLAH'),'ji@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Jordan','Stern','JSASASASASA', password('BLAH'),'js@email.com');
+values ('Jordan','Stern','JSASASASASA', password('BLAHBLAH'),'js@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Kuan','Jiang','KJASASASASA', password('BLAH'),'kj@email.com');
+values ('Kuan','Jiang','KJASASASASA', password('BLAHBLAH'),'kj@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','aASASASASA', password('BLAH'),'mkdl@email.com');
+values ('Georges','Mathieu','aASASASASA', password('BLAHBLAH'),'mkdl@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','bASASASASA', password('BLAH'),'ab@email.com');
+values ('Georges','Mathieu','bASASASASA', password('BLAHBLAH'),'ab@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','cASASASASA', password('BLAH'),'ac@email.com');
+values ('Georges','Mathieu','cASASASASA', password('BLAHBLAH'),'ac@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','dASASASASA', password('BLAH'),'ad@email.com');
+values ('Georges','Mathieu','dASASASASA', password('BLAHBLAH'),'ad@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','eASASASASA', password('BLAH'),'ae@email.com');
+values ('Georges','Mathieu','eASASASASA', password('BLAHBLAH'),'ae@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','fASASASASA', password('BLAH'),'af@email.com');
+values ('Georges','Mathieu','fASASASASA', password('BLAHBLAH'),'af@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','gASASASASA', password('BLAH'),'ag@email.com');
+values ('Georges','Mathieu','gASASASASA', password('BLAHBLAH'),'ag@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','hASASASASA', password('BLAH'),'ah@email.com');
+values ('Georges','Mathieu','hASASASASA', password('BLAHBLAH'),'ah@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','iASASASASA', password('BLAH'),'aj@email.com');
+values ('Georges','Mathieu','iASASASASA', password('BLAHBLAH'),'aj@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','jASASASASA', password('BLAH'),'ak@email.com');
+values ('Georges','Mathieu','jASASASASA', password('BLAHBLAH'),'ak@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','kASASASASA', password('BLAH'),'al@email.com');
+values ('Georges','Mathieu','kASASASASA', password('BLAHBLAH'),'al@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','lASASASASA', password('BLAH'),'am@email.com');
+values ('Georges','Mathieu','lASASASASA', password('BLAHBLAH'),'am@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','mASASASASA', password('BLAH'),'an@email.com');
+values ('Georges','Mathieu','mASASASASA', password('BLAHBLAH'),'an@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','nASASASASA', password('BLAH'),'ao@email.com');
+values ('Georges','Mathieu','nASASASASA', password('BLAHBLAH'),'ao@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','oASASASASA', password('BLAH'),'ap@email.com');
+values ('Georges','Mathieu','oASASASASA', password('BLAHBLAH'),'ap@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','pASASASASA', password('BLAH'),'aq@email.com');
+values ('Georges','Mathieu','pASASASASA', password('BLAHBLAH'),'aq@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','qASASASASA', password('BLAH'),'ar@email.com');
+values ('Georges','Mathieu','qASASASASA', password('BLAHBLAH'),'ar@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','rASASASASA', password('BLAH'),'as@email.com');
+values ('Georges','Mathieu','rASASASASA', password('BLAHBLAH'),'as@email.com');
 
 insert into student (FirstName, LastName, PermenantCode, `password`, email) 
-values ('Georges','Mathieu','sASASASASA', password('BLAH'),'at@email.com');
+values ('Georges','Mathieu','sASASASASA', password('BLAHBLAH'),'at@email.com');
 
 
 Show columns from addresses;
