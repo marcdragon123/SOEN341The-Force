7c97002f8cbd23ed0752193ba0a345af96cb6219
CCnator <olivier.cc@hotmail.com>
2016-03-18 12:46:08 -0400

    Sections
    This to insert info into Sections table

--- DBScripts/TimeSlotInsert.sql
+++ DBScripts/TimeSlotInsert.sql
@@ -1,8 +1,5 @@
 use soen341;
 
-insert into sections(course_Master_List_id,Section, Semester) values 
-(((select 1 id from course_master_list),(SUBSTRING(MD5(RAND()) FROM 1 FOR 2),"Fall")));
-
 select Section, course_Master_List_id 
 from Sections;
 
