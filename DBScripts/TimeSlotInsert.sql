ad65e1aed599a66eff38da072465a7f4ec85a929
CCnator <olivier.cc@hotmail.com>
2016-03-19 11:10:22 -0400

    Just fixing a Timeslot problem

--- DBScripts/TimeSlotInsert.sql
+++ DBScripts/TimeSlotInsert.sql
@@ -1,9 +1,4 @@
 use soen341;
 
-select Section, course_Master_List_id 
-from Sections;
-
-insert into Timeslot(Sections_Section, Sections_course_Master_List_id) 
-select Section, course_Master_List_id 
-from Sections;
+select * from timeslot;
 
