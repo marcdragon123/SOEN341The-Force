7beb56a10f293cb277d0afbacee60bc6f33a29b3
CCnator <olivier.cc@hotmail.com>
2016-04-03 12:59:21 -0400

    Quick Changes

---
+++ DBScripts/DummyPrereqs.sql
@@ -0,0 +1,7 @@
+use soen341;
+INSERT INTO prereq(MainCourseID, PrereqCourseID)Values('1','7');
+INSERT INTO prereq(MainCourseID, PrereqCourseID)Values('2','8');
+INSERT INTO prereq(MainCourseID, PrereqCourseID)Values('3','9');
+INSERT INTO prereq(MainCourseID, PrereqCourseID)Values('4','10');
+INSERT INTO prereq(MainCourseID, PrereqCourseID)Values('5','11');
+INSERT INTO prereq(MainCourseID, PrereqCourseID)Values('6','12');
