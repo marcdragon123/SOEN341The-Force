13c9a211379734cb05a46ed370cabfe6a983714a
PopMyStack <julian.ippolito@hotmail.com>
2016-04-07 20:48:50 -0400

    validateEmail fix

--- site/js/emailValidSignUp.js
+++ site/js/emailValidSignUp.js
@@ -1,6 +1,6 @@
 //Valid email
 email = document.getElementById("email");
-function validateEmail(inputText)  
+function validateEmail()  
 {  
 		var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(email.value.match(mailformat))
