eb9331acec85751c42781cf9c9049c8ae55a2ac0
wolfcall <gmathieu6@gmail.com>
2016-04-09 12:54:50 -0400

    Prereqs added

---
+++ DBScripts/Prereqs.sql
@@ -0,0 +1,36 @@
+Use soen341;
+
+Select * from course_master_list order by course_code, number;
+
+Select * from prereq;
+
+Insert into prereq values (2,5);
+Insert into prereq values (1,18);
+Insert into prereq values (7,13);
+Insert into prereq values (10,13);
+Insert into prereq values (5,9);
+Insert into prereq values (1,10);
+Insert into prereq values (4,15);
+Insert into prereq values (6,15);
+Insert into prereq values (2,22);
+Insert into prereq values (4,22);
+Insert into prereq values (6,22);
+Insert into prereq values (11,31);
+Insert into prereq values (2,8);
+Insert into prereq values (13,29);
+Insert into prereq values (1,16);
+Insert into prereq values (5,16);
+Insert into prereq values (11,17);
+Insert into prereq values (10,17);
+Insert into prereq values (17,19);
+Insert into prereq values (17,20);
+Insert into prereq values (19,20);
+Insert into prereq values (20,23);
+Insert into prereq values (19,25);
+Insert into prereq values (11,21);
+Insert into prereq values (17,21);
+Insert into prereq values (4,30);
+Insert into prereq values (6,30);
+Insert into prereq values (23,26);
+Insert into prereq values (25,26);
+Insert into prereq values (26,27);
