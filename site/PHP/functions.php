7255c40709fc2b63a09be545f6449b63d2e10aa1
PopMyStack <julian.ippolito@hotmail.com>
2016-04-09 20:07:31 -0400

    Fixed garbage can positioning

--- site/PHP/functions.php
+++ site/PHP/functions.php
@@ -312,7 +312,7 @@ function loadTable(){
 		if (count($DOW)==2){
 			//course code + num and then lectures
 			echo '<tr>'
-			.'<td rowspan=2><div class = "delete"></div>'.$row['Course_code'].' '.$row['number'].'</td>'
+			.'<td rowspan=2><div class = "delete" input type = "submit"></div> &nbsp;'.$row['Course_code'].' '.$row['number'].'</td>'
 			. '<td>Lecture:'.getFullDay(getDayStr($DOW[0])).getFullDay(getDayStr($DOW[1])).'-'.$row['start'].'-'.$row['end'].'</td>';
 		}
 		elseif (count($DOW)==1) {
