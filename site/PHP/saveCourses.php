53e823ee11e378b685258b6340eeaee42a8b9d85
Joey <joeytedeschi@Joeys-MBP.zyxel.com>
2016-04-11 23:19:01 -0400

    Courses marked as saved on Account page are saved on submit

--- site/PHP/saveCourses.php
+++ site/PHP/saveCourses.php
@@ -6,11 +6,11 @@ $conn = getCon();
 $userId = $_SESSION["loginID"];
 $array = $_POST['courses'];
 
-echo $userId;
-
-foreach ($array as $color){
-echo $color."<br />";
-
+foreach ($array as $value) {
+    $query = "INSERT INTO transcripts (Completed, Enrollment_Student_idStudent, Enrollment_Sections_course_master_List_id) VALUES (true, '$userId','$value')";
+    mysqli_query($conn, $query);
 }
+
+header("Location: ../Account.php");
 
 ?>
